01c6d7868c771f9ed5f5dd3fafe1f3f7d1f39185
Kacper Pabian <lui3pytoficial@gmail.com>
2025-10-19 21:24:26 +0200

    Implement FizzBuzz logic in FizzBuzz.ps1

---
+++ FizzBuzz.ps1
@@ -0,0 +1,19 @@
+param(
+[int]$max = 30
+)
+$tab = @()
+for($i = 1;$i -le $max;$i++0){
+  if($i % 5 -eq 0 -and $i % 3 -eq 0){
+    $tab += "FizzBuzz"
+    }
+  elseif($i % 3 -eq 0){
+    $tab += "Fizz"
+    }
+   elseif($i % 5 -eq 0){
+    $tab += "Buzz"
+    }
+    else{
+      $tab += $i
+      }
+  }
+    
