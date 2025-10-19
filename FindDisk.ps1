e04af79f50700fcab57c41dacb56695a3cd29ba2
Kacper Pabian <lui3pytoficial@gmail.com>
2025-10-19 21:09:10 +0200

    Fix function definition for FindDisk

--- FindDisk.ps1
+++ FindDisk.ps1
@@ -1,3 +1,3 @@
-function FindDisk{
+
   Get-CimInstance -ClassName "Win32_LogicalDisk" | Select-Object Name,@{Name ="Space(GB)";Expression = {[math]::Round($_.Size/1GB,2)}}, @{Name ="FreeSpace(GB)"; Expression = {[math]::Round($_.FreeSpace/1GB,2)}}
-  }
+
